42e9b7cc7f083406a0b8717ec277640269929ed0
Olivier VILLAVERDE <ovslash@hotmail.fr>
2025-10-20 17:01:37 +0200

    v1.0.0 — état initial propre

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,9 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/trame.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Ftrame.mdx&astroContentModuleFlag=true")],
+["src/content/docs/bdd/postgres.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbdd%2Fpostgres.mdx&astroContentModuleFlag=true")],
+["src/content/docs/bdd/psql.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fbdd%2Fpsql.mdx&astroContentModuleFlag=true")],
+["src/content/docs/methodes/git.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmethodes%2Fgit.mdx&astroContentModuleFlag=true")],
+["src/content/docs/methodes/gitflow.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fmethodes%2Fgitflow.mdx&astroContentModuleFlag=true")]]);
+		
